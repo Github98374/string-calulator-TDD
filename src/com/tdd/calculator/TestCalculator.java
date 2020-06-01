bb2a73d9c80015bc5de66ea942a49c5047dfa72a
Github98374 <brijtechbussiness@gmail.com>
2020-06-01 12:31:22 +0530

    added test cases for handling negative data, over thousand values and delimiter

--- src/com/tdd/calculator/TestCalculator.java
+++ src/com/tdd/calculator/TestCalculator.java
@@ -13,23 +13,52 @@ public class TestCalculator {
 	}
 
 	@Test
-	public void testEmptyStringArgument() {
+	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
 
 	@Test
-	public void testOneArgument() {
+	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
 
 	@Test
-	public void testTwoArgument() {
+	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
 	}
 
 	@Test
-	public void testThreeArgument() {
+	public void testThreeNumbers() {
 		assertEquals(6, Calculator.add("1,2,3"));
 	}
 
+	@Test
+	public void testNewLine() {
+		assertEquals(6, Calculator.add("1\n2,3"));
+	}
+
+	@Test
+	public void testNegativeNumver() {
+		try {
+			Calculator.add("-1,2");
+		} catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(), "Negatives not allowed: -1");
+		}
+
+		try {
+			Calculator.add("2,-4,3,-5");
+		} catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(), "Negatives not allowed: -4,-5");
+		}
+	}
+
+	@Test
+	public void testOverThousand() {
+		assertEquals(2, Calculator.add("1000,2"));
+	}
+
+	@Test
+	public void testOtherDelimiter() {
+		assertEquals(3, Calculator.add("//;\n1;2"));
+	}
 }
